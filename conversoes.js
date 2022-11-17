8205f5f2296836e5b2a18a7c8419ed8af39314df
Afonso Gomes <afsnot15@gmail.com>
2022-11-17 15:40:57 -0300

    Aula 2

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+//Conversão implícita
+
+const numero = 456;
+const numerString = "456";
+
+console.log(numero == numerString);
+
+console.log(numero + numerString);
+
+//coversão explícita
+
+//Number()
+//String()
+
+console.log(numero + Number(numerString));
---
+++ truthy-false.js
@@ -0,0 +1,30 @@
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+
+//0 = false
+//1 = true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+//null == vazio ou nada (tipo de dado)
+//undefined nao atribuido
+
+let variavel; //undefined
+let variavelNull = null //null
+
+console.log(variavel);
+console.log(variavelNull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof variavel);
+console.log(typeof variavelNull); //object
+
