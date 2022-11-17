951448f69e33b59a03073ef1924356a2ad8579a6
Afonso Gomes <afsnot15@gmail.com>
2022-11-17 14:44:07 -0300

    Aula 1

---
+++ type-number.js
@@ -0,0 +1,8 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1.5;
+const segundoNumero = 2.5;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo!';
+const senha = "SenhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Afonso";
+
+console.log(citacao + meuNome);
+
+const cifrao = '\u2705'
+
+console.log(cifrao);
+
+//template string ou template literal
