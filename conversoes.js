770c315477ee425d574144de984191c5a14e2864
Afonso Gomes <afsnot15@gmail.com>
2022-11-18 14:14:34 -0300

    Conversoes

--- conversoes.js
+++ conversoes.js
@@ -13,3 +13,13 @@ console.log(numero + numerString);
 //String()
 
 console.log(numero + Number(numerString));
+
+
+    {
+        var respostaDeTudo = 3.14
+        let idade = 42
+        const pi = 29
+        console.log(respostaDeTudo, idade, pi)
+    
+    }
+    console.log(respostaDeTudo, idade, pi)
