91315d53448f9151a039efa89b7ce1d9fcad8d75
Afonso Gomes <afsnot15@gmail.com>
2022-11-20 09:01:44 -0300

    Funcoes

---
+++ expressoes-funcao.js
@@ -0,0 +1,10 @@
+//Expressão de função
+const soma = function(num1, num2){return num1 + num2}
+console.log(soma(1, 2))
+
+console.log(apresentar());
+
+
+function apresentar(){
+    return "olá";
+}
---
+++ funcoes.js
@@ -0,0 +1,17 @@
+
+
+function imprimioTexto(texto) {
+    console.log(texto);
+}
+
+imprimioTexto("oi");
+imprimioTexto("outro texto");
+
+function soma() {
+    return 2 + 2;
+}
+
+console.log(soma());
+
+imprimioTexto(soma());
+
